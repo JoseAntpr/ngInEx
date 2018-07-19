279541c388834dccbdd5dd2e83866c298e2b9eb8
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-19 12:41:09 +0200

    ADD: show items in template

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AppState } from '../../app.reducer';
+import { Store } from '@ngrx/store';
+import { IngresoEgreso } from '../ingreso-egreso.model';
+import { Subscription } from '../../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.css']
 })
-export class DetalleComponent implements OnInit {
+export class DetalleComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  items: IngresoEgreso[];
+  subscription: Subscription;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('ingresoEgreso')
+        .subscribe( ingresoEgreso => {
+          console.log(ingresoEgreso.items);
+          this.items = ingresoEgreso.items;
+        });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  deleteItem(uid: string) {
+    console.log(uid);
   }
 
 }
